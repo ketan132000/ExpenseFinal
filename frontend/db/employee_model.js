b1b2c83a3d93743851d46b1f33206d47b7a1b9ac
Ketan Chawla <80246514+ketan132000@users.noreply.github.com>
2022-08-17 20:10:32 +0530

    Delete frontend/db directory

--- frontend/db/employee_model.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require("mongoose");
-
-let employeeSchema = new mongoose.Schema({
-    olmId: { type: String, required: true, index: true, unique: true },
-    expenses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Expense" }],
-});
-
-module.exports = mongoose.model("Employee", employeeSchema);
